77781a65d83e2d145fd07d2422eab285b02dfbf7
shoaibshaikh0333 <shoaibshaikh0333@gmail.com>
2024-09-21 20:11:46 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+class person{
+    eat() {
+        console.log("eat");
+    }
+}
+
+Sleep() { {
+    console.log("sleep");   
+}
+
+}
+
+class Engineer{
+    work() {
+        console.log("solve problems,build something");
+    }
+}
+let shoaibobj = new Engineer();
